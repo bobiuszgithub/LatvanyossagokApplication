21d23d8faff88861f850030840b690624926d6dd
Bence <71759654+bobiuszgithub@users.noreply.github.com>
2020-12-01 12:23:59 +0100

    csatlakozás az adatbázishoz

--- LatvanyossagokApplication/Form1.cs
+++ LatvanyossagokApplication/Form1.cs
@@ -1,20 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data.SqlTypes;
 using System.Windows.Forms;
-
 namespace LatvanyossagokApplication
 {
     public partial class Form1 : Form
     {
+
+        MySqlConnection conn;
+
         public Form1()
         {
             InitializeComponent();
+            conn = new MySqlConnection("Server = localhost; Database = latvanyossagokdb; Uid = root; Pwd = ");
+            conn.Open();
+            this.FormClosed += (sender, args) =>
+            {
+                if (conn != null)
+                {
+                    MessageBox.Show("Hiba a kapcsolódás során!");
+                    conn.Close();
+                }
+
+            };
         }
     }
 }
